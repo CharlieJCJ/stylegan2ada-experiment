142a681d9c05c534d5b786623579e4db43c514a2
Charlie Cheng-Jie Ji <charliechengjieji@berkeley.edu>
2022-08-05 02:00:58 -0700

    job

--- job_files/0to6job.sh
+++ job_files/0to6job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 2 x v100 in savio3_gpu, 8 x cpu
+# 4 x GTX2080TI in savio3_gpu, 8 x cpu
 # Job name:
-#SBATCH --job-name=0to6job
+#SBATCH --job-name=2to6job
 #
 # Account:
 #SBATCH --account=fc_psychvis
@@ -13,22 +13,21 @@
 #SBATCH --nodes=1
 #
 # Number of tasks (one for each GPU desired for use case) (example):
-#SBATCH --ntasks=2
+#SBATCH --ntasks=4
 #
-#SBATCH -q v100_gpu3_normal
 # Processors per task:
 # Always at least twice the number of GPUs (savio2_gpu and GTX2080TI in savio3_gpu)
 # Four times the number for TITAN and V100 in savio3_gpu
 # Eight times the number for A40 in savio3_gpu
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #
 #Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
-#SBATCH --gres=gpu:V100:2
+#SBATCH --gres=gpu:GTX2080TI:4
 #
-# Wall clock limit:
+# Wall clock limit (2 days):
 #SBATCH --time=1-00:00:00
 #
 ## Command(s) to run (example):
 echo "Running on host"
 nvidia-smi
-python /global/scratch/users/charliechengjieji/stylegan2ada-experiment/train.py --data=/global/scratch/users/charliechengjieji/stylegan2ada-experiment/data/0to6.zip --outdir=/global/scratch/users/charliechengjieji/stylegan2ada-experiment/result
+python /global/scratch/users/charliechengjieji/stylegan2ada-experiment/train.py --gpus=4 --data=/global/scratch/users/charliechengjieji/stylegan2ada-experiment/data/0to6.zip --outdir=/global/scratch/users/charliechengjieji/stylegan2ada-experiment/result
